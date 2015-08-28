b25f90f2a34453c5d86dbd5b6db61bb68ab53ae5
swapnilrmahale <swapnilrmahale@gmail.com>
2015-08-28 14:55:56 +0530

    Commiting

--- js/test.js
+++ js/test.js
@@ -2,47 +2,16 @@ var menuTimeout = null;
 
 function mouseMoveHandler(e) {
 
-	if (e.pageX < 200) { // add hover condition
+	if (e.pageY < 50) { // add hover condition
     	clearTimeout(menuTimeout);
     	menuTimeout = null;
-    	showLMenu();
-
-  	} else if (e.pageX > ($(window).width()-200)) {
-  		clearTimeout(menuTimeout);
-    	menuTimeout = null;
-    	showRMenu();
+      $('.menu').slideDown('slow', function() {});
 
   	} else if (menuTimeout === null) {
-    	menuTimeout = setTimeout(hideMenu(), 30000);
+    	menuTimeout = setTimeout(function() {
+        $('.menu').slideUp('slow', function() {});
+      }, 1000);
   }
-}
-
-function showLMenu() {
-  	/*$('#lMenu').animate({
-        left: "2vw"
-   	}, 1000);*/
-   	$('#lMenu').fadeIn(2000);
-}
-
-function hideMenu() {
-	console.log('hide');
-  	/*$('#lMenu').animate({
-        left: "-50vw"
-  	}, 1000);
-
-  	$('#rMenu').animate({
-        left: "150vw"
-  	}, 1000);*/
-	$('#rMenu').fadeOut(500);
-	$('#lMenu').fadeOut(500);
-
-}	
-
-function showRMenu() {
-  	/*$('#rMenu').animate({
-        left: "88vw"
-   	}, 1000);*/
-   	$('#rMenu').fadeIn(2000);
 }
 
 $(function() {
